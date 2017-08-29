1a244d348e0eeea5e462d679ede0fb1ae985dc17
jgreenQA <jack.green@qa.com>
2017-08-29 16:24:06 +0000

    Added Task 1 and Task 2

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,4 +9,7 @@ class Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def values(val1: Option[String] = Option("test"), val2: Option[Int] = Option(0)) = Action { implicit request =>
+    Ok(views.html.index(s"Value 1 is: $val1\nValue 2 is $val2"))
+  }
 }
